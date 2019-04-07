28cc8173ba1fde7f61da9d0bf88bb264f0b2009a
Vijay <Vijay@Vijay-Nagaraju>
2019-04-07 15:23:36 +0530

    trial upload

---
+++ MavenDemo/src/main/java/generic/BaseTest.java
@@ -0,0 +1,38 @@
+package generic;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public abstract class BaseTest implements AutoConstant
+{
+	public WebDriver driver;
+	static
+	{
+		System.setProperty(chrome_key, chome_value);
+		System.setProperty(gecko_key, gecko_value);
+	}
+	@BeforeMethod
+	public void preCondition()
+	{
+		driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://demo.actitime.com/login.do");		
+	}
+	@AfterMethod
+	public void postCondition(ITestResult res)
+	{
+		int status=res.getStatus();
+		if(status==2)
+		{
+			String name=res.getName();
+			GenericUtils.getScreenShot(driver, name);
+		}
+		driver.close();
+	}
+}
